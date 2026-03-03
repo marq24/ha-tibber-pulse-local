cbdecc8e14713f848a6782a926394730edbc8b9d
marq24 <marquardt24@gmail.com>
2026-03-03 22:31:13 +0100

    unify ws handling

--- custom_components/tibber_local/__init__.py
+++ custom_components/tibber_local/__init__.py
@@ -139,6 +139,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     if not init_succeeded: #or coordinator.data is None:
         raise ConfigEntryNotReady
     else:
+        hass.data[DOMAIN][config_entry.entry_id] = coordinator
+        await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
         if use_websocket:
             # ws watchdog...
             if hass.state is CoreState.running:
@@ -146,10 +149,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
             else:
                 hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, coordinator.start_watchdog)
 
-        hass.data[DOMAIN][config_entry.entry_id] = coordinator
-        await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
         config_entry.async_on_unload(config_entry.add_update_listener(entry_update_listener))
-
         return True
 
 
